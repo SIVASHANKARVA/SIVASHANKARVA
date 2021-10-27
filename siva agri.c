a10a97ea4bb8fc48493512e3c389f02c51c7a449
SIVASHANKARVA <92291063+SIVASHANKARVA@users.noreply.github.com>
2021-10-27 11:55:21 +0530

    Add files via upload

---
+++ siva agri.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+//variable declaration
+int phy, che, mat, eng,tot;
+//initialization
+
+phy = 40;
+che = 50;
+mat = 60;
+eng = 70;
+//calculation
+
+tot = phy+che+mat+eng;
+//output
+
+printf("marks in phy:%d\n",phy);
+printf("marks in che:%d\n",che);
+printf("marks in mat:%d\n",mat);
+printf("marks in eng:%d\n",eng);
+printf("total marks:%d",tot);
+
+
