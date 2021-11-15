fa7fd18de6f9c462a6875e53b3102153a0d58b6c
SIVASHANKARVA <92291063+SIVASHANKARVA@users.noreply.github.com>
2021-11-15 12:20:34 +0530

    Add files via upload

---
+++ four num program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("enter four numbers")
+	printf("produc of", (a*b*c*d));
+	printf("product of %d,%d, and %d is %d", a,b,c,d (a*b*c*d));
+	
+}
